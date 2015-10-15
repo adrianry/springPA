00fa5a48eb82d468bf01c69f346f4d39c92561f4
Adrian Ryser <adrian.ryser@zkb.ch>
2015-10-15 15:44:39 +0200

    first commit

---
+++ src/main/java/org/adi/springPA/CamundaBpmProcessApplication.java
@@ -0,0 +1,37 @@
+package org.adi.springPA;
+
+import org.camunda.bpm.application.PostDeploy;
+import org.camunda.bpm.application.PreUndeploy;
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+import org.camunda.bpm.engine.ProcessEngine;
+import org.camunda.bpm.engine.ProcessEngines;
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.runtime.ProcessInstance;
+
+@ProcessApplication
+public class CamundaBpmProcessApplication extends ServletProcessApplication  {
+
+	private ProcessEngine processEngine;
+
+	@PostDeploy
+	public void startup(ProcessEngine engine) {
+		this.processEngine = engine;
+	    System.out.println("\n\n --------->process engine name:" + processEngine.getName());
+		starteProzess();
+	}
+
+
+	public void starteProzess(){
+		ProcessEngine engine = ProcessEngines.getDefaultProcessEngine();
+		RuntimeService runtimeService = engine.getRuntimeService();
+		ProcessInstance instance = runtimeService.startProcessInstanceByKey("springPA","adi1");
+		//runtimeService.signalEventReceived("adisSignal");
+		System.out.println("\n ---------> Instance erzeugt: " + instance.getId());
+	}
+	  
+	@PreUndeploy
+	public void stopAndUndeploy() {
+		  System.out.println("\n\n ---------->stop and shutdown process application now");
+	}
+}
---
+++ src/main/java/org/adi/springPA/exampleBean.java
@@ -0,0 +1,23 @@
+package org.adi.springPA;
+
+public class exampleBean {
+
+    protected boolean invoked;
+
+
+    public void invoke() {
+        this.invoked = true;
+        System.out.println(getClass().getName() + " is currently invoked.");
+    }
+
+    public boolean isInvoked() {
+        System.out.println("\n\n -------->" + getClass().getName() + " is invoked. Id is: ");
+        return invoked;
+    }
+
+    public void setInvoked(boolean invoked) {
+        this.invoked = invoked;
+        System.out.println(getClass().getName() + " set invoked=" + invoked);
+    }
+
+}
