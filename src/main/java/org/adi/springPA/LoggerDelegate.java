556b84d40b50a32982b58149840926bf889c89e3
Adrian Ryser <adrian.ryser@zkb.ch>
2015-10-19 17:03:13 +0200

    comments

--- src/main/java/org/adi/springPA/LoggerDelegate.java
+++ src/main/java/org/adi/springPA/LoggerDelegate.java
@@ -1,28 +1,25 @@
 package org.adi.springPA;
 
-import java.util.logging.Logger;
-
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 
-/**
- * This is an empty service implementation illustrating how to use a plain Java 
- * class as a BPMN 2.0 Service Task delegate.
- */
+import java.util.logging.Logger;
+
 public class LoggerDelegate implements JavaDelegate {
  
   private final Logger LOGGER = Logger.getLogger(LoggerDelegate.class.getName());
   
   public void execute(DelegateExecution execution) throws Exception {
     
-    LOGGER.info("\n\n  ... LoggerDelegate invoked by "
+    LOGGER.info("\n ************************************************************************************************************** " +
+            "\n  ... LoggerDelegate invoked by "
             //+ "processDefinitionId=" + execution.getProcessDefinitionId()
             //+ ", activtyId=" + execution.getCurrentActivityId()
             + ", activtyName='" + execution.getCurrentActivityName() + "'"
             //+ ", processInstanceId=" + execution.getProcessInstanceId()
             + ", businessKey=" + execution.getProcessBusinessKey()
             //+ ", executionId=" + execution.getId()
-            + " \n\n");
+            + " \n ************************************************************************************************************** \n");
     
   }
 
--- src/main/java/org/adi/springPA/Transaktion.java
+++ src/main/java/org/adi/springPA/Transaktion.java
@@ -1,17 +1,17 @@
 package org.adi.springPA;
 
-import java.util.logging.Logger;
-
 import org.camunda.bpm.engine.delegate.BpmnError;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+import java.util.logging.Logger;
 
 public class Transaktion implements JavaDelegate {
 	 
 	  private final Logger LOGGER = Logger.getLogger(LoggerDelegate.class.getName());
 	  
 	  public void execute(DelegateExecution execution) throws Exception {
-		  LOGGER.info("\n\n Transaktion.class ist ausgelöst worden. \n\n"); 
+		  LOGGER.info("\n ******************************************************* \n Transaktion.class ist ausgeloest worden. \n ******************************************************* \n");
 		  throw new BpmnError("ADIS_ERROR");
 	  }
 
