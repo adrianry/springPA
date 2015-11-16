6d470adef007718957e1178035c8dcb12f8dd85c
Adrian Ryser <adrian.ryser@zkb.ch>
2015-11-16 16:04:57 +0100

    sachen einfacher gemacht

--- src/main/java/org/adi/springPA/CamundaBpmProcessApplication.java
+++ src/main/java/org/adi/springPA/CamundaBpmProcessApplication.java
@@ -7,9 +7,7 @@ import org.camunda.bpm.application.impl.ServletProcessApplication;
 import org.camunda.bpm.engine.ProcessEngine;
 import org.camunda.bpm.engine.ProcessEngines;
 import org.camunda.bpm.engine.RuntimeService;
-import org.camunda.bpm.engine.TaskService;
 import org.camunda.bpm.engine.runtime.ProcessInstance;
-import org.camunda.bpm.engine.task.Task;
 
 @ProcessApplication
 public class CamundaBpmProcessApplication extends ServletProcessApplication  {
@@ -29,18 +27,19 @@ public class CamundaBpmProcessApplication extends ServletProcessApplication  {
 		RuntimeService runtimeService = engine.getRuntimeService();
 		ProcessInstance instance = runtimeService.startProcessInstanceByKey("springPA","nixkey");
 		System.out.println("\n ---------> Instance erzeugt: " + instance.getId() + "\n");
-		//runtimeService.signalEventReceived("adisSignal");
-		System.out.println("\n ---------> Signal gesendet! Schlafe fuer 30 sekunden \n");
-		Thread.sleep(30000);
-		System.out.println("\n ---------> Aufwachen und User Task erledigen! \n");
-		TaskService taskservice = engine.getTaskService();
-		Task task = taskservice.createTaskQuery().processInstanceId(instance.getId()).singleResult();
-		task.setOwner("demo");
-		task.setAssignee("demo");
-		System.out.println("\n ---------> Task Owner: "+ task.getOwner() +" \n");
-		System.out.println("\n ---------> Task Assignee: "+ task.getAssignee() +" \n");
+	//	runtimeService.signalEventReceived("adisSignal");
+	//	System.out.println("\n ---------> Signal gesendet! \n");
+	//	Thread.sleep(30000);
+	//	System.out.println("\n ---------> Aufwachen und User Task erledigen! \n");
+	//	TaskService taskservice = engine.getTaskService();
+	//	Task task = taskservice.createTaskQuery().processInstanceId(instance.getId()).singleResult();
+	//	task.setOwner("demo");
+	//	task.setAssignee("demo");
+	//	taskservice.saveTask(task);
+	//	System.out.println("\n ---------> Task Owner: "+ task.getOwner() +" \n");
+	//	System.out.println("\n ---------> Task Assignee: "+ task.getAssignee() +" \n");
 	//	taskservice.complete(task.getId());
-		System.out.println("\n ---------> Task erledigt und Methode starteProzess fertig! \n");
+	//	System.out.println("\n ---------> Task assigned to Demo User\n");
 	}
 	  
 	@PreUndeploy
